2dd68a8c490d11367a0f5b640d620e850d15c59c
junpil park <iamfeel2@gmail.com>
2020-12-09 23:27:35 +0900

    fix : DebugTree -> Timber.DebugTree

--- app/src/main/java/com/example/clone/archtecture/todo/TodoApplication.kt
+++ app/src/main/java/com/example/clone/archtecture/todo/TodoApplication.kt
@@ -23,6 +23,6 @@ class TodoApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        if(BuildConfig.DEBUG) Timber.plant(DebugTree())
+        if(BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 }
